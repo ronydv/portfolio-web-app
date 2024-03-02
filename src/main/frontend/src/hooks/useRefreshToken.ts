70713ed68fb77069579c792ebb90198e7fd77f95
celsiusdv <celsius.dev@protonmail.com>
2024-03-01 23:34:05 -0300

    added cookies to the response for refresh-tokens

--- src/main/frontend/src/hooks/useRefreshToken.ts
+++ src/main/frontend/src/hooks/useRefreshToken.ts
@@ -2,16 +2,16 @@ import axios from "axios";
 import { useAuthContext } from "./useAuthContext";
 
 const useRefreshToken = () => {
-    let { auth, setAuth }: UserContext = useAuthContext();
+    let { setAuth }: UserContext = useAuthContext();
 
     //this will return a user and a token, when use this method, make sure whether you need the token or user with token
     const refresh = async (): Promise<LoginResponse> => {
-        const response = await axios.post<LoginResponse>('/api/v1/auth/refresh-token',
-            auth.token, {// sends the authenticated user's refresh token to be verified in the server
-            headers: {
+        const response = await axios.post<LoginResponse>('/api/v1/auth/refresh-token',{
+            headers: {// sends the authenticated user's refresh token to be verified in the server
                 "Accept": "application/json",
                 "Content-Type": "application/json",
             },
+            withCredentials:true//sends the cookies to the endpoint
         });
         //the response contains user with roles, accessToken and refreshToken
         setAuth(response.data);
